c06aafc8ec1b9adec55a982461a1adf5d6ba7dc4
Aori Nevo <aori.nevo@bloomingdales.com>
2018-08-25 14:44:51 -0400

    I-1: Add devly-hosts plugin.

--- plugins/index.js
+++ plugins/index.js
@@ -1,8 +1,13 @@
 const {store} = require('@devly/devly-store');
 const {addApacheConfig, addApacheCommands} = require('@devly/devly-apache/actions');
+const {addHostsConfig, addHostsCommands} = require('@devly/devly-hosts/actions');
 const {dispatch} = store;
 
 require('@devly/devly-apache');
+require('@devly/devly-hosts');
 
-dispatch(addApacheConfig(require('./manifests/apache')));
+dispatch(addApacheConfig(require('../manifests/apache')));
+dispatch(addHostsConfig(require('../manifests/hosts')));
+
 dispatch(addApacheCommands());
+dispatch(addHostsCommands());
