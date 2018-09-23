d7894838436c208a4e126090a69cb93cda787c67
Aori Nevo <aori.nevo@bloomingdales.com>
2018-09-23 11:14:46 -0400

    I-1: Update example.

--- manifests/wordpress.js
+++ manifests/wordpress.js
@@ -1,7 +1,21 @@
+const DNS = 'local.wordpress.aorinevo.org';
+const PORT = 8080;
+
 module.exports = {
   databaseName: 'wordpress',
   wordpressUsername: 'wordpress',
-  port: 8080,
+  port: PORT,
   hostname: 'localhost',
-  projectPath: '/Users/aorinevo/Repositories/WordPress'
+  projectPath: '/Users/aorinevo/Repositories/WordPress',
+  vHosts: `
+  # WordPress Virtual Host
+
+  <VirtualHost *:80>
+    ProxyPreserveHost off
+    ServerName ${DNS}
+    ServerAlias www.${DNS}
+
+    ProxyPass / http://${DNS}:${PORT}/
+  </VirtualHost>
+  `
 };
